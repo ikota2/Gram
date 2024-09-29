fc5730b483ed26e7cd18bceabe61bb74b2d3f706
Ivan Kotov <polyzay@gmail.com>
2024-09-29 16:33:23 +0200

    Initial Commit

---
+++ GrammaTestOne/GrammaTestOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrammaTestOneApp.swift
+//  GrammaTestOne
+//
+//  Created by ivan caiman on 29.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct GrammaTestOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
